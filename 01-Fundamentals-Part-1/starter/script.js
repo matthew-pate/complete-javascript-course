e9b8b550983b39e0f8f00670356704d835c54731
matthew-pate <mattpate@gmail.com>
2020-11-23 21:37:41 -0600

    firrst commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+let firstName = "Bob";
+console.log(firstName);
+
+let PI = 3.1415
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
